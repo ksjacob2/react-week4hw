4bbb6dab531ecf36fababf67b685c436873ed0e4
ksjacob2 <102990304+ksjacob2@users.noreply.github.com>
2024-03-19 22:37:50 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Weather from "./Weather";
-import axios from "axios";
-
-import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
